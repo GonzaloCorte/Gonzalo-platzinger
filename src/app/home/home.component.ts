2c14d7412d03dba8be946760334c6d108a1bed9d
Gonzalo Corte <gonzalo.corte@proveedor.ues21.edu.ar>
2019-10-24 15:20:44 -0300

    agregando user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/User';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users = [
+    {nick: 'Eduardo', subnick: 'Mi mensaje personal', status: 'online', age: 28, email: 'eduardo@platzi.com', friend: true},
+    {nick: 'Yuliana', subnick: 'Mi mensaje personal', status: 'busy', age: 25, email: 'yuliana@platzi.com', friend: true},
+    {nick: 'Freddy', subnick: 'Mi mensaje personal', status: 'away', age: 28, email: 'freddy@platzi.com', friend: false},
+  ];
+
+  friends: User [];
+  constructor() {
+    let user1: User = {
+      nick: 'Eduardo',
+      subnick: 'ElEdu',
+      age: 32,
+      email: 'aa@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let user2: User = {
+      nick: 'Pedro',
+      subnick: 'pepe',
+      age: 19,
+      email: 'aa1@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let user3: User = {
+      nick: 'Josefa',
+      subnick: 'jochi',
+      age: 24,
+      email: 'aa2@aa.com',
+      friend: true,
+      uid: 1
+    };
+    let user4: User = {
+      nick: 'Marta',
+      subnick: 'martu',
+      age: 30,
+      email: 'aa4@aa.com',
+      friend: false,
+      uid: 1
+    };
+    let user5: User = {
+      nick: 'Mamerto',
+      subnick: 'mame',
+      age: 31,
+      email: 'aa45@aa.com',
+      friend: false,
+      uid: 1
+    };
+
+    this.friends = [user1, user2, user3, user4, user5];
+  }
 
   ngOnInit() {
   }
